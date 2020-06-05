c8651c8a30882e1abc6a62c60e94d5e4211ee6ac
nullscc <jarves@foxmail.com>
2020-06-05 16:05:22 +0800

    application of stack: line edit

---
+++ stack/line_edit.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<assert.h>
+#include"stack_sq.cpp"
+
+
+
+int main() {
+	SqStack S;
+	InitStack(S);
+	char ch;
+	int tmp;
+	while (true) {
+		// scanf("%c", &ch);
+		ch = getchar(); // scanf will jump space, tab, \n and EOF, so here must be function getchar
+		if (ch == EOF) {printf("EOF.....\n"); break;}
+		
+		if (ch == '#') Pop(S, tmp);
+		else if (ch == '@') ClearStack(S);
+		else if (ch == '\n') {
+			printf("let following string writen to file:\n");
+			PrintStack(S);
+			ClearStack(S);
+		}
+		else Push(S, ch);
+	}
+	printf("content in stack:\n");
+	PrintStack(S);
+	printf("\nend...\n");
+
+	return 0;
+}
--- stack/stack_sq.cpp
+++ stack/stack_sq.cpp
@@ -49,3 +49,23 @@ Status Pop(SqStack &S, SElemType &e) {
 	e = *--S.top;
 	return OK;
 }
+
+Status ClearStack(SqStack &S) {
+	S.top = S.base; // NOTE: 这里有一次错误的将其写成S.base=S.top，结果总是报realloc invalid pointer，因为realloc的第一个参数必须要是malloc返回的等堆区的地址，所以不能乱改S.base的地址
+
+	return OK;	
+}
+
+Status PrintStack(SqStack &S) {
+	if (S.base == S.top) {
+		return ERROR;
+	}
+
+	int tmp;
+	SElemType *s_base = S.base;
+	while (s_base < S.top) {
+		printf("%c", *s_base++);
+	}
+
+	return OK;	
+}
