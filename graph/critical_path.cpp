78fd5c5d7d0f64a01a613e7e85fcc5a1734bb85b
nullscc <jarves@foxmail.com>
2020-06-15 23:09:01 +0800

    some critical comment

--- graph/critical_path.cpp
+++ graph/critical_path.cpp
@@ -1,9 +1,20 @@
-// 搞不动两个问题: 1. 多条关键路径怎么办，能输出吗？ 2. 算法中的栈可以改成队列吗？
+/*
+搞懂几个问题: 
+1. 多条关键路径怎么办，能输出吗？ 
+	能输出，看data6的数据，算法找的是关键活动(由于是AOE，即是边)，所以所有的关键的边都能找出来
+2. 算法中的栈可以改成队列吗？
+	不可以，改成队列的话ltv算不对，导致算法出错
+3. 算法中的etv, ltv, ete, lte分别怎么算出来的？
+	见下面的注释
+4. 算法有一个有点违背常识的地方(理解这个非常重要):
+	终点事件的最早发生时间=终点事件的最晚发生时间；关键在于理解: ltv[g.vexnum-1]为什么等于etv[g.vexnum-1]；主要是因为终点的最早发生时间其实就是耗时最长的路径了，所以终点的最晚发生时间就是最早能发生的时间，说起来很奇怪，用数据 v0,v1,2 v0,v2,2 v1,v3,5 v2,v3,2就知道了，etv[3]的值不是4而是7
+	}
+
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include"../common.h"
 #include"../stack/stack_sq.cpp"
-#include"../queue/link_queue.cpp"
 
 #define MAXSIZE 100
 
@@ -91,7 +102,7 @@ Status TopoSort(Graph &g, SqStack &SC, int *etv) {
 		while(p) {
 			if(!(--g.vex[p->v].in)) Push(S, p->v);
 
-			if(etv[v]+p->weight > etv[p->v]) {		// 这里对连接到p-v的每个进行遍历，找到一条时间消耗最长的路径，就是这个事件的最早开始时间
+			if(etv[v]+p->weight > etv[p->v]) {		// NOTE: 顺推，这里对连接到p-v的每个进行遍历，找到一条时间消耗最长的路径，就是这个事件的最早开始时间
 				etv[p->v] = etv[v] + p->weight;
 			}
 			p = p->next;
@@ -118,16 +129,17 @@ Status CriticalPath(Graph &g) {
 
 
 	for(i=0; i<g.vexnum; i++) {
-		ltv[i] = etv[g.vexnum-1];	// 由于最后一个结点是终点，所以耗费时间肯定最长，事件的最晚发生时间不能再最后一个事件已经发生了还没发生，所以最多可以等于最后一个时间的最早发生时间
+		ltv[i] = etv[g.vexnum-1];	// NOTE: 这个语句有两个目的，1. 将ltv[0..g.vexnum-1]置为最大的数，好在计算ltv时比小 2. 将ltv[g.vexnum-1]
+									// 其实关键在于理解: ltv[g.vexnum-1]为什么等于etv[g.vexnum-1]；其实主要是因为终点的最早发生时间其实就是耗时最长的路径了，所以终点的最晚发生时间就是最早能发生的时间，说起来很奇怪，用数据 v0,v1,2 v0,v2,2 v1,v3,5 v2,v3,2就知道了，etv[3]的值不是4而是7
 	}
 
 	int v;
-	while(!IsEmpty(SC)) {
-		Pop(SC, v);
+	while(!IsEmpty(SC)) {			// NOTE: 如果换成队列，这里的ltv会算不对；因为ltv必须是从终点往源头逆推才能算出来
+		Pop(SC, v);					// 这里第一个pop出来的就是终点
 		pTNode p = g.vex[v].first;
 		while(p) {
 			if(ltv[p->v]-p->weight < ltv[v]) {
-				ltv[v] = ltv[p->v] - p->weight;		// NOTE: 为什么？
+				ltv[v] = ltv[p->v] - p->weight;		// NOTE: 为什么？逆推，画出两个从v出发到p->v终止的结点就知道了；取最小的原因在于需要预留足够长的时间给最需要时间的那个活动
 			}
 			p = p->next;
 		}
@@ -136,7 +148,7 @@ Status CriticalPath(Graph &g) {
 		pTNode p = g.vex[i].first;
 		while(p) {
 			ete = etv[i];
-			lte = ltv[p->v]-p->weight;				// NOTE: 为什么？
+			lte = ltv[p->v]-p->weight;				// NOTE: 为什么？ 逆推，其实是按递推关系来算的，以vk->vj为例，这个活动的最晚发生时间也要在vj的最晚发生时间减去vk->vj这个活动所花费的时间
 			if(ete == lte) {
 				printf("V%c->V%c 耗费时间:%d\n", g.vex[i].e, g.vex[p->v].e, p->weight);
 			}
@@ -149,6 +161,7 @@ Status CriticalPath(Graph &g) {
 
 // 数据使用《大话数据结构》p305(电子书p281), 图7-9-4
 // 执行 a.out < data5 即可
+// 执行 a.out < data6 看有两条关键路径的情况
 int main() {
 	Graph g;
 	GetVex(g);
