3aa6359825ddaf273867c78109d908ea6cc789fc
nullscc <jarves@foxmail.com>
2020-06-16 17:45:07 +0800

    good version for bst algorithm

--- search/bst.cpp
+++ search/bst.cpp
@@ -1,4 +1,5 @@
-// 二叉排序树不能有相同的数值？
+// 二叉排序树不能有相同的数值？ yes，但是可根据实际应用对数据的存储和逻辑做调整
+// 注意删除结点的操作不要费力的去找要删除的结点的双亲结点，见相应的注释
 #include<stdio.h>
 #include<stdlib.h>
 #include<assert.h>
@@ -17,32 +18,29 @@ typedef struct Node{
 
 // Search要返回两种信息: 成功或者失败；最后一次有效结点
 // 即成功时需要返回对应的搜索到的结点；失败时需要返回失败时的父节点
-Status Search(pNode &T, pNode f, pNode &p, ElemType e, pNode &parent) {
+Status Search(pNode &T, pNode f, pNode &p, ElemType e) {
 	int ret;
 
 	if(!T) {
 		p = f;
-		parent = f;
 		return ERROR;
 	}
 
 	if(T->data == e) {
 		p = T;
-		parent = f;
 		return OK;
 	}
 
 	if(e < T->data) {
-		return Search(T->lchild, T, p, e, parent);
+		return Search(T->lchild, T, p, e);
 	} else {
-		return Search(T->rchild, T, p, e, parent);
+		return Search(T->rchild, T, p, e);
 	}
 }
 
 Status Insert(pNode &T, ElemType e) {
 	pNode s;
-	pNode parent;
-	if (!(Search(T, NULL, s, e, parent))) {
+	if (!(Search(T, NULL, s, e))) {
 		pNode p = (pNode)malloc(sizeof(Node));
 		p->data = e;
 		p->lchild = NULL;
@@ -67,62 +65,6 @@ Status Insert(pNode &T, ElemType e) {
 	return OK;
 }
 
-Status SearchLchildRchildLast(pNode &T, pNode &f, pNode &last){
-	if (!T) return ERROR;
-	
-	if(!T->lchild) return ERROR;
-
-	pNode p = T->lchild;
-	while(p) {
-		if (p->rchild && !p->rchild->rchild) {
-			f = p;
-			last = p->rchild;
-		}
-		p = p->rchild;
-	}
-
-	return OK;
-}
-
-Status Delete(pNode &T, ElemType e) {
-	pNode s, parent;
-	int ret = Search(T, NULL, s, e, parent);			// 处理parent为NULL的情况太麻烦了，so ugly
-	if(!ret) return ERROR;
-	printf("搜索:%d,它的父结点为:%d, 搜索到的结点:%d\n", e, parent->data, s->data);
-
-	if (!s->lchild) {
-		if(s->data < parent->data) {
-			parent->lchild = s->rchild;
-		} else {
-			parent->rchild = s->rchild;
-		}
-	} else if(!s->rchild) {
-		if(s->data < parent->data) {
-			parent->lchild = s->lchild;
-		} else {
-			parent->rchild = s->lchild;
-		}
-
-	} else {
-		pNode f, last;
-		SearchLchildRchildLast(s, f, last);
-		if(last->lchild) {
-			f->rchild = last->lchild;
-		} else {
-			f->rchild = NULL;
-		}
-		last->lchild = s->lchild;
-		last->rchild = s->rchild;
-		if(s->data > parent->data) {
-			parent->rchild = last;
-		} else {
-			parent->lchild = last;
-		}
-	}
-	free(s);
-	return OK;
-}
-
 Status print_elem(ElemType e) {
 	printf("%d ", e);
 	return OK;
@@ -134,6 +76,41 @@ Status MidSearch(pNode pnode) {
 	print_elem(pnode->data);
 	MidSearch(pnode->rchild);
 	return OK;
+}
+
+Status Delete(pNode &p) {
+	pNode q, s;
+
+	if(!p->rchild) {
+		q = p;
+		p = p->lchild;
+		// NOTE: 这里很重要，看了好久才看懂，还以为大话数据结构和严蔚敏书写错了呢...
+		// 实际上p本身是虽然是要删除的结点的地址，但是这个地址值是存放在其双亲结点的lchild(或rchild)域里面，又因为这里传过来的p是引用，所以相当于直接更改了双亲结点的lchild(或rchild)域
+		// 所以根本不用费力去找要删除结点的双亲结点
+		free(q);
+	} else if(!p->lchild) {
+		q = p;
+		p = p->rchild;
+		free(q);
+	} else {
+		q = p;
+		s = p->lchild;
+		while(s->rchild) {		// NOTE: 这里就要找双亲结点了，最终q为双亲结点，s为要删除的结点
+			q=s; s=s->rchild;
+		}
+		p->data = s->data;
+		if(q!=p) q->rchild = s->lchild;		// 如果要替换的结点有右子树
+		else q->lchild = s->lchild;			// 如果要替换的结点没有右子树
+		free(s);
+	}
+	return OK;
+}
+
+Status DeleteBST(pNode &T, ElemType e) {
+	if (!T) return ERROR;
+	if(e == T->data) return Delete(T);
+	else if(e < T->data) return DeleteBST(T->lchild, e);
+	else return DeleteBST(T->rchild, e);
 }
 
 
@@ -149,9 +126,13 @@ int main() {
 	MidSearch(T);
 	printf("\n");
 
-	Delete(T, 62);
+	pNode p;
+	Search(T, NULL, p, 99); 
+	
+	DeleteBST(T, 62);
 	printf("删除一个数据以后树的数据:");
 	MidSearch(T);
 	printf("\n");
+	
 	return 0;
 }
