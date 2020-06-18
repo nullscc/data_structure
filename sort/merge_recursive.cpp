faf1b42d57a780e2653d13fad055a688628d827b
nullscc <jarves@foxmail.com>
2020-06-18 18:10:02 +0800

    for debug

--- sort/merge_recursive.cpp
+++ sort/merge_recursive.cpp
@@ -1,13 +1,12 @@
 #include<stdio.h>
-
-#define MAXSIZE 100
-
-int M[100];
+#include<stdlib.h>
 
 void merge(int *t, int *l, int i, int mid, int j) {
 	int m=i, n=mid;
 	int index = i;
+	printf("merge, index:%d i:%d mid:%d j:%d\n", index, i, mid, j);
 	while(m<=mid && n<=j) {
+		printf("merge in while m:%d mid:%d n:%d j:%d, index:%d, t[n]:%d\n", m, mid, n, j, index, t[n]);
 		if(t[m]<t[n]) {
 			l[index] = t[m];
 			index++;
@@ -24,34 +23,45 @@ void merge(int *t, int *l, int i, int mid, int j) {
 		m++;
 	}
 	while(n<=j) {
-		l[index] = t[m];
+		l[index] = t[n];
 		index++;
-		m++;
+		n++;
 	}
 }
 
+int tmp[100];
 void sort(int *t, int *l, int i, int j) {
-	printf("wtf111....%d %d\n", i, j);
-	int tmp[100];
+	printf("tmp:%p\n", l);
 	if(i==j) {
+		printf("2222 tmp:%p\n", l);
 		l[j] = t[j];
+		printf("j:%d t[j]:%d l[j]:%d\n", j, t[j], l[j]);
 	}
 	else {
 		int mid = (i+j)/2;
-		sort(l, tmp, i, mid);
-		sort(l, tmp, mid+1, j);
+		sort(t, tmp, i, mid);
+		sort(t, tmp, mid+1, j);
 		merge(tmp, l, i, mid, j);
 	}
 }
 
 int main() {
-	int l[9] = {9, 1, 5, 8, 3, 7, 4, 6, 2};
-	sort(l, l, 0, 8);
+	int l[3] = {9, 1, 5};
+	sort(l, l, 0, 2);
 
 	int i;
 	printf("排序后的序列:");
-	for(i=0; i<9; i++) {
+	for(i=0; i<3; i++) {
 		printf("%d ", l[i]);
 	}
 	printf("\n");
+
+	// int l[9] = {9, 1, 5, 8, 3, 7, 4, 6, 2};
+	// sort(l, l, 0, 8);
+	// int i;
+	// printf("排序后的序列:");
+	// for(i=0; i<9; i++) {
+	// 	printf("%d ", l[i]);
+	// }
+	// printf("\n");
 }
